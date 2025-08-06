5dc09d9cf0aa773c293b219ebdba38f1cd4586e7
aaron-henao <aaronhenao27@gmail.com>
2025-08-06 07:42:50 -0500

    tests dos

--- calculadora.py
+++ calculadora.py
@@ -4,14 +4,14 @@ def cuota_mensual(propiedad,prestamo, tasa, plazo):
   
 def prueba_caso_normal_1():
 
-    propiedad = 350_000_000
+    propiedad = 350000000
     prestamo = 40/100
     tasa = 10/100
     plazo = 15
 
     cuota_calculada = cuota_mensual(propiedad,prestamo, tasa, plazo )
 
-    cuota_esperada = 1_504_447
+    cuota_esperada = 1504447.16
 
     if(round(cuota_calculada,0) == round(cuota_esperada,0)):
         print("prueba exitosa¡")
@@ -20,14 +20,14 @@ def prueba_caso_normal_1():
 
 
 def prueba_caso_normal_2():
-    propiedad = 200_000_000
+    propiedad = 200000000
     prestamo = 35/100
     tasa = 11/100
     plazo = 12
 
     cuota_calculada = cuota_mensual(propiedad,prestamo, tasa, plazo )
 
-    cuota_esperada = 1_504_447
+    cuota_esperada =  87748968.89
 
     if(round(cuota_calculada,2) == round(cuota_esperada,2)):
         print("prueba exitosa¡")
@@ -44,13 +44,62 @@ def prueba_caso_normal_3():
 
     cuota_calculada = cuota_mensual(propiedad,prestamo, tasa, plazo )
 
-    cuota_esperada = 2995140_66
+    cuota_esperada = 2995140.66
 
     if(round(cuota_calculada,2) == round(cuota_esperada,2)):
         print("prueba exitosa¡")
     else:
         print("prueba fallida¡")
+
+def maximo_permitido():
+    propiedad = 1500000000
+    prestamo = 60/100
+    tasa = 12/100
+    plazo = 25
+
+    cuota_calculada = cuota_mensual(propiedad,prestamo, tasa, plazo )
+
+    cuota_esperada = 9479017.00
+
+    if(round(cuota_calculada,2) == round(cuota_esperada,2)):
+        print("prueba exitosa¡")
+    else:
+        print("prueba fallida¡")
+
+
+
+def minimo_permitido():
+    propiedad = 80000000
+    prestamo = 30/100
+    tasa = 12/100
+    plazo = 5
+
+    cuota_calculada = cuota_mensual(propiedad,prestamo, tasa, plazo )
+
+    cuota_esperada = 304022.86
+
+    if(round(cuota_calculada,2) == round(cuota_esperada,2)):
+        print("prueba exitosa¡")
+    else:
+        print("prueba fallida¡")
+def plazo_minimo():
+    propiedad = 600000000
+    prestamo = 50/100
+    tasa = 12/100
+    plazo = 5
+
+    cuota_calculada = cuota_mensual(propiedad,prestamo, tasa, plazo )
+
+    cuota_esperada = 6597782.21
+
+    if(round(cuota_calculada,2) == round(cuota_esperada,2)):
+        print("prueba exitosa¡")
+    else:
+        print("prueba fallida¡")
+
 prueba_caso_normal_1()
 prueba_caso_normal_2()
 prueba_caso_normal_3()
-
+maximo_permitido()
+minimo_permitido()
+plazo_minimo()
